f3287bb292501005500ef5b01e222eb0e3eee19b
Berserk9000 <cruz.d.kevin@gmail.com>
2022-07-17 16:25:17 -0400

    finishing up HTML stylesheets and getting variables ready

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+function startPage() {
+    var cityEl = document.getElementById("enter-city");
+    var searchEl = document.getElementById("search-button");
+    var clearEl = document.getElementById("clear-history");
+    var nameEl = document.getElementById("city-name");
+    var currentPicEl = document.getElementById("current-pic");
+    var currentTempEl = document.getElementById("temperature");
+    var currentHumidityEl = document.getElementById("humidity");
+    var currentWindEl = document.getElementById("wind-speed");
+    var currentUVEl = document.getElementById("UV-index");
+    var historyEl = document.getElementById("history");
+    var fivedayEl = document.getElementById("fiveday-header");
+    var todayweatherEl = document.getElementById("today-weather");
+    var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+    // unique API
+    var APIKey = "c9171a22ca52ca8877ccb46ef06fe2f9";
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+startPage();
